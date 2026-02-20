f374c0aa3a0193639f99504cadaa87312c89f77d
FatmaElMahdi1000 <f69246984@gmail.com>
2026-02-20 15:26:53 +0200

    coding.in.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import Pack1.OPR;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Welcome!");
+        int a  = 3;
+        int b = 2;
+        //object creation
+        OPR obj = new OPR();
+        int res = obj.summation(a, b);
+        System.out.println("The sum result: " + res);
+        //division object
+        OPR obj2 = new OPR();
+        float res2 = obj2.division(a, b);
+        System.out.println("The division result = " + res2);
+
+
     }
 }
---
+++ src/Pack1/OPR.java
@@ -0,0 +1,15 @@
+package Pack1;
+
+public class OPR {
+
+    public int summation(int a, int b)
+    {
+        return a + b;
+    }
+
+    public float division(float a, float b)
+    {
+        return a/b;
+    }
+
+}
