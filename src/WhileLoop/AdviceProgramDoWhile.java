d8fa91124f33903280449e5d7eaafef75b81e91b
FatmaElMahdi1000 <f69246984@gmail.com>
2026-03-18 12:54:36 +0200

    Coding.in.JAVA!

---
+++ src/WhileLoop/AdviceProgramDoWhile.java
@@ -0,0 +1,38 @@
+package WhileLoop;
+import java.util.ArrayList;
+import java.util.Scanner    ;
+import java.util.Random;
+
+public class AdviceProgramDoWhile {
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add(0, "No answer, Sorry!");
+        answers.add(0, "Hard Question!");
+        answers.add(0, "Ask GPT!Sorry!");
+        answers.add(0, "Think again about the question, you'll find the answer yourself!");
+        System.out.println(answers);
+        String ans1;
+        String question;
+        Scanner obj = new Scanner(System.in);
+        Random rnd = new Random();
+        do{
+            System.out.println("Do you've a question? ");
+            ans1 = obj.nextLine().toLowerCase().trim();
+            if(!ans1.equals("yes")) //force to break, to not move with the next part
+            {
+                break;
+            }
+            System.out.println("Ok, Go ahead, what is it? ");
+            question = obj.nextLine();
+            System.out.println("The Answer to your question is: ");
+            //REVIEW
+            String element = answers.get(rnd.nextInt(answers.size()));
+            System.out.println(element);
+        }
+        while(true);
+        obj.close();
+    }
+
+}
--- src/WhileLoop/AverageValue.java
+++ src/WhileLoop/AverageValue.java
@@ -7,17 +7,19 @@ public class AverageValue {
     public static void main(String[] args)
     {
         Scanner obj = new Scanner(System.in);
-        ArrayList<Integer> arrList = new ArrayList<>();
-        int i = 0;
-        int num;
+        ArrayList<Double> arrList = new ArrayList<>();
+        int i = 1;
+        double num;
+        double sum = 0;
         while(i <= 5)
         {
             System.out.println("Please Enter a number: ");
             num = obj.nextInt();
+            sum+= num;
             arrList.add(num);
             i++;
         }
-        System.out.println("The list is = " + arrList);
-
+        System.out.println("The list is = " + arrList + "\n" +"The Average:" + sum/arrList.size());
+        obj.close();
     }
 }
