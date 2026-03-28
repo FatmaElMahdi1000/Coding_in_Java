671d9c1a92612b9e7fa81a80fe325d00fc70bd25
FatmaElMahdi1000 <f69246984@gmail.com>
2026-03-29 01:30:37 +0200

    Coding.in.JAVA!

---
+++ src/Basics/DecToBinary.java
@@ -0,0 +1,31 @@
+package Basics;
+import java.util.Scanner;
+public class DecToBinary {
+
+    public static void main(String[] args)
+    {
+        double DecNum;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a decimal number to convert: ");
+        DecNum = obj.nextDouble();
+        String Bin = "";
+        //REVIEW : StringBuilder
+
+
+        double DivRes = DecNum / 2; //5 / 2 = 2.5
+//        double rem = DecNum % 2; // 5 % 2 = 1.
+        while(DivRes != 0.00)
+        {
+            double rem =  DecNum % 2;
+            //converting the num to string and concatenating it.
+            Bin += String.valueOf((int) rem);
+            DecNum = (int) DivRes;
+
+            DivRes = DecNum / 2;
+
+        }
+        String BinReversed = new StringBuilder(Bin).reverse().toString();
+        System.out.println(BinReversed);
+
+    }
+}
