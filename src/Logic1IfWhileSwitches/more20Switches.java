650685a198b4051dd01f714fdd9f3ae262dc7d4c
FatmaElMahdi1000 <f69246984@gmail.com>
2026-03-30 01:38:07 +0200

    Coding.in.JAVA!

--- src/Logic1IfWhileSwitches/more20Switches.java
+++ src/Logic1IfWhileSwitches/more20Switches.java
@@ -1,4 +1,22 @@
 package Logic1IfWhileSwitches;
-
+import java.util.Scanner;
 public class more20Switches {
+
+    public static void main(String[] args)
+    {
+        Scanner obj2 = new Scanner(System.in);
+        int num;
+        System.out.print("Enter a number: ");
+        num = obj2.nextInt();
+        boolean res = num % 20 == 1 || num % 20 == 2;
+        switch(res)
+        {
+            case true:
+                System.out.println(true);
+                break;
+            case false:
+                System.out.println(false);
+                break;
+        }
+    }
 }
