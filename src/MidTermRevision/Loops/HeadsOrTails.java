22718b08583d84f9738e6e884c0b732c8e54ab01
FatmaElMahdi1000 <f69246984@gmail.com>
2026-04-07 10:49:30 +0200

    Coding.in.JAVA!

--- src/MidTermRevision/Loops/HeadsOrTails.java
+++ src/MidTermRevision/Loops/HeadsOrTails.java
@@ -1,4 +1,20 @@
 package MidTermRevision.Loops;
+import java.util.Random;
+import java.util.Scanner;
+public class HeadsOrTails
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        Random rnd = new Random();
+        //exclusive
+        int res = rnd.nextInt(0,2);
+        System.out.println(res);
+        System.out.print("Enter a guess: ");
+        int guess = obj.nextInt();
 
-public class HeadsOrTails {
+        //ternary operator
+        String result = (guess == res) ? "Correct" : "Incorrect";
+        System.out.println(result);
+    }
 }
