8ab2521d25afb7f2bb720f2189c3ae0ff9ac9869
FatmaElMahdi1000 <f69246984@gmail.com>
2026-05-05 19:12:32 +0300

    Coding In Java

---
+++ src/OOPRelationships/Customer.java
@@ -0,0 +1,115 @@
+package OOPRelationships;
+import java.util.*;
+//Association
+public class Customer
+{
+    private String CustName;
+    private double PurchaseAmount;
+
+    //Non Parametrized Constructor for(building an array of objects)
+    Customer()
+    {
+
+    }
+
+    public Customer(double PurchaseAmount, String CustName)
+    {
+        this.CustName = CustName;
+        this.PurchaseAmount = PurchaseAmount;
+    }
+    //Can buy the book or not??
+    //this is where the association appear, an object of another class passed to a method of a different class.
+    String purchasingStatus(Book book)
+    {
+        if(PurchaseAmount >= book.getPrice())
+        {
+            return "The Customer Named: " + this.CustName + " is able to buy this book"+"\n"
+                    + book.getBookInfo();
+        }
+        else
+        {
+            return "Unfortunately, You don't have enough money to buy this book"
+                    +"\n" + "You need extra $" + (book.getPrice()-PurchaseAmount);
+
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //Adding Customer Manually
+        Customer customer1 = new Customer(13.0, "Fatma");
+        Book book1 = new Book("Rules of Life", 20.00);
+        System.out.println(book1.getBookInfo());
+        System.out.println(customer1.purchasingStatus(book1));
+
+        //prompt the user to provide several customers' data
+        Customer[] custList = new Customer[2];
+        Book[] bookList = new Book[2];
+
+        for(int i =0; i <custList.length;i++)
+        {
+            custList[i] = new Customer();
+            System.out.print("Enter the Name of the Customer#"+(i+1) +":");
+            String name = sc.nextLine();
+            custList[i].CustName = name;
+            sc.nextLine(); // Clear the buffer so the next name isn't skipped!
+            System.out.print("Enter the Purchase Amount of the Customer#"+(i+1) +":");
+            Double amount = sc.nextDouble();
+            custList[i].PurchaseAmount = amount;
+            System.out.println("Customer: "+custList[i].CustName + " has only $"+custList[i].PurchaseAmount);
+        }
+        for(int j = 0; j <bookList.length; j++)
+        {
+            bookList[j] = new Book();
+            System.out.print("Enter the Book Price: ");
+            Double BkPrice = sc.nextDouble();
+            //REVIEW
+            bookList[j].setPrice(BkPrice);
+            System.out.print("Enter the Book Title: ");
+            bookList[j].setTitle(sc.nextLine());
+        }
+
+    }
+
+}
+class Book
+{
+    private String title;
+
+    private Double price;
+
+    Book()
+    {
+
+    }
+    public Book(String title, Double price)
+    {
+        this.title= title;
+        this.price = price;
+    }
+
+    String getBookInfo()
+    {
+        return "The Book entitled: " + this.title + " costs: "+this.price + "$";
+    }
+    //to access both Price and Title in a different class, we need to use setters(Mutators) and Getters(Accessors)
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
+
